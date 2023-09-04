4d6e5637f0c4ddc36aef91b94d2d8d4686bb2178
seunghwan11 <songmagic22@gmail.com>
2023-09-05 03:41:47 +0900

    feat : [006-CUSTOMER_balance] 고객에 대한 잔액 조회 구현

--- user-api/src/main/java/com/zerobase/cms/user/domain/model/Customer.java
+++ user-api/src/main/java/com/zerobase/cms/user/domain/model/Customer.java
@@ -42,6 +42,8 @@ public class Customer extends BaseEntity{
     private String verificationCode;
     private boolean verify;
 
+    private Integer balance;
+
     public static Customer from(SignUpForm form){
         return Customer.builder()
             .email(form.getEmail().toLowerCase(Locale.ROOT))
